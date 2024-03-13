debb4bc8b21ecc44d205e124f6778fd2f8301336
jjvargass <jotavargas89@gmail.com>
2024-03-13 18:59:46 -0500

    ajuste nombre de campos con estandar y puestos de trabajo faltantes

--- models/crm_lead.py
+++ models/crm_lead.py
@@ -7,27 +7,27 @@ class Lead(models.Model):
     _name = "crm.lead"
     _inherit = "crm.lead"
 
-    liena_negocio = fields.Many2one(
+    linea_negocio_id = fields.Many2one(
         string='Linea de Negocio',
         comodel_name='crm.lead.business.line',
         help='''Linea de Negocio Agata''',
     )
-    tipo_solucion = fields.Many2one(
+    tipo_solucion_id = fields.Many2one(
         string='Tipo de Solucion',
         comodel_name='crm.lead.type.solution',
         help='''Tipo de Solucion Agata''',
     )
-    aliado = fields.Many2one(
+    aliado_id = fields.Many2one(
         string='Aliado',
         comodel_name='crm.lead.ally',
         help='''Aliado Agata''',
     )
-    estrategia = fields.Many2one(
+    estrategia_id = fields.Many2one(
         string='Estrategia',
         comodel_name='crm.lead.strategy',
         help='''Estrategia Agata''',
     )
-    presupuesto = fields.Many2one(
+    presupuesto_id = fields.Many2one(
         string='Presupuesto',
         comodel_name='crm.lead.budget',
         help='''Presupuesto Agata''',
